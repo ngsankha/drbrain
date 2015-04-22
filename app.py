8be2f34c68eae46f1981ce86793dfc8554a28535
Sankha Narayan Guria <sankha93@gmail.com>
2015-04-22 19:26:11 +0530

    Change the homepage

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def login():
-    return render_template('login.html')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
